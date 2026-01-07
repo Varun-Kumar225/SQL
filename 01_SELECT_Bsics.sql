9777b9dcbf08972f40751bacae91c0940de9fe91
Varun-Kumar225 <varunkumarmitcse@gmail.com>
2026-01-07 21:55:21 +0530

    Add queries for Sweden, Norway, Denmark, and area

--- 01_SELECT_Bsics.sql
+++ 01_SELECT_Bsics.sql
@@ -5,3 +5,11 @@
 SELECT population
 FROM world
 WHERE name = 'Germany';
+
+-- Q2: Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway','Denmark');
+
+-- Q3: show the country and the area for countries with an area between 200,000 and 250,000.
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
